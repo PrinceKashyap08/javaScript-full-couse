bd112dc2899c64fe5ec36e37240a3a3ce961b191
Prince <vasukashyap08@gmail.com>
2024-02-10 22:44:00 +0530

    commit

---
+++ variables and Data types/main.js
@@ -0,0 +1,74 @@
+console.log("vasu kashyap");
+
+
+// var x = 6;
+// var y = 8;
+// var z = x  +  y;
+
+// console.log(z)
+
+
+// var x = 10;
+// var x = 20;
+
+// console.log(x)
+
+
+// let x = 8;
+// let y = 10;
+// let z = x + y;
+
+// console.log(z)
+
+
+// let x = 10;
+// x = 20;
+// console.log(x)
+// console.log(typeof(x))
+
+
+// const price1 = 100;
+// const price2 = 500;
+// let total = price1 - price2;
+
+// console.log(total);
+
+// const str1 = " prince kashyap";
+// const str2 = "vasu kashyap";
+// console.log(str2 + str1)
+// console.log(typeof(str1 + str2));
+
+
+// const obj1 = {a: 0, b: 1};
+// const obj2 = {b: 0, c: 1};
+// const obj3 = {c: 0, d: 1};
+
+// Object.assign(obj1, obj2, obj3)
+// console.log(obj1);
+// console.log(typeof(obj1));
+
+
+// const userName = 'Prince';   /*String*/ 
+// const age = 28; /* Number */
+// const x = null; /* null */
+// const z = undefined; /* undefined */
+// const youFollow = false ; /* boleen */
+
+const Student = {
+    fullName: 'Prince kashyap',
+    age:  28,
+    Profession:'Frontent devloper',
+    cgpa: 70.2,
+    student: true,
+};
+ 
+// console.log(Student);
+// console.log(Student.fullName);
+// console.log(Student['student']);
+
+
+
+
+
+
+
